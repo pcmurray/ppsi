bc7ef015eb6a9eb87ef03eb12f25bcf7d702a5f6
Cesar Prados <c.prados@gsi.de>
2014-10-05 19:41:57 +0200

    pdelay/pp-inst: add ts for tx and rx of the sync msg, then
    I will keep the t1..t4 for calculating the
    peer path delay and these new are used only
    for synchronization
    
    Signed-off-by: C.Prados <c.prados@gsi.de>

--- include/ppsi/pp-instance.h
+++ include/ppsi/pp-instance.h
@@ -150,7 +150,7 @@ struct pp_instance {
 	struct pp_net_path np;
 
 	/* Times, for the various offset computations */
-	TimeInternal t1, t2, t3, t4;			/* *the* stamps */
+	TimeInternal t1, t2, t3, t4, t5, t6;			 /* *the* stamps */
 	TimeInternal cField;				/* transp. clocks */
 	TimeInternal last_rcv_time, last_snt_time;	/* two temporaries */
 
