fc23da11d0674ee1805cdcead5388bd266d564b6
José Luis Gutiérrez <jlgutierrez@ugr.es>
2015-06-03 16:48:39 +0200

    p2p:arch defining htobe64 for 64 bits correctionfield
    correction field shall use 64 bits and htobe64 shall be used instead of
    ntohl

--- arch-bare-x86-64/include/arch/arch.h
+++ arch-bare-x86-64/include/arch/arch.h
@@ -14,6 +14,17 @@
 	(((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) |	\
 	(((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24))
 
+#  define htobe64(x) __bswap_64 (x)
+#define __bswap_constant_64(x) \
+     ((((uint64_t)(x)  &  (uint64_t)0xff00000000000000ul)>> 56)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x00ff000000000000ul)>>	40)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x0000ff0000000000ul)>> 24)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x000000ff00000000ul)>>  8)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x00000000ff000000ul)<<  8)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x0000000000ff0000ul)<< 24)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x000000000000ff00ul)<< 40)		|	\
+       (((uint64_t)(x) &  (uint64_t)0x00000000000000fful)<< 56))
+
 #define ntohs htons
 #define ntohl htonl
 
