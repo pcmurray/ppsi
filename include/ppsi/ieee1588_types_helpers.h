5eea4a4623806d2f313e4e135ead229d32cbf5dc
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-04-20 14:52:38 +0200

    headers: introduce helper functions for ieee1588 types
    These are most of all internal<->on-wire conversion functions and accessors
    for on-wire types.
    We place these functions in a separate file because we get a build error
    on arch-wrpc otherwise:
    
    * Helpers need htons, ntohs, ... , which are declared in arch/arch.h
    * wrpc's arch.h includes proto-ext-whiterabiit/wr-api.h, which needs
      struct pp_instance
    * struct pp_instance is declared in ppsi/ppsi.h, but ppsi.h includes
      ieee1588_types before struct pp-instance.h

---
+++ include/ppsi/ieee1588_types_helpers.h
@@ -0,0 +1,48 @@
+#ifndef __IEEE1588_TYPES_HELPERS_H__
+#define __IEEE1588_TYPES_HELPERS_H__
+
+#include <ppsi/ieee1588_types.h>
+#include <arch/arch.h>
+
+static inline void uint48_internal_to_wire(uint48_wire *dst,
+					   const uint64_t *src)
+{
+	dst->lsdw = htonl(*src & 0xffffffffULL);
+	dst->msw = htons(*src >> 32);
+}
+
+static inline void uint48_wire_to_internal(uint64_t *dst,
+					   const uint48_wire *src)
+{
+	*dst = ntohl(src->lsdw) + (((uint64_t)ntohs(src->msw)) << 32);
+}
+
+static inline void timestamp_internal_to_wire(timestamp_wire *dst,
+					      const Timestamp *src)
+{
+	uint48_internal_to_wire(&dst->secondsField, &src->secondsField);
+	dst->nanosecondsField = htonl(src->nanosecondsField);
+}
+
+static inline void timestamp_wire_to_internal(Timestamp *dst,
+					      const timestamp_wire *src)
+{
+	uint48_wire_to_internal(&dst->secondsField, &src->secondsField);
+	dst->nanosecondsField = ntohl(src->nanosecondsField);
+}
+
+static inline void time_internal_to_timestamp_internal(Timestamp *dst,
+						       const TimeInternal *src)
+{
+	dst->secondsField = src->seconds;
+	dst->nanosecondsField = src->nanoseconds;
+}
+
+static inline void timestamp_internal_to_time_internal(TimeInternal *dst,
+						       const Timestamp *src)
+{
+	dst->seconds = src->secondsField;
+	dst->nanoseconds = src->nanosecondsField;
+}
+
+#endif /* __IEEE1588_TYPES_HELPERS_H__ */
