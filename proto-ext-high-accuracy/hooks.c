29d2407a4437cc432a5754e8802c31286c8fc6d0
Alessandro Rubini <rubini@gnudd.com>
2016-09-03 14:36:44 +0200

    ha: added hooks to rely on wr-servo when L1SYNC_UP
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- proto-ext-high-accuracy/hooks.c
+++ proto-ext-high-accuracy/hooks.c
@@ -26,6 +26,9 @@ static int ha_init(struct pp_instance *ppi, unsigned char *pkt, int plen)
 	pp_diag(ppi, ext, 2, "hook: %s -- ext %i\n", __func__,
 		ppi->cfg.ext);
 
+	/* ext_data is the wr-servo */
+	ppi->ext_data = ppi->glbs->global_ext_data;
+
 
 #ifdef CONFIG_ARCH_WRPC
 	/* HACK: we can't get the value here (but WR manages to) */
@@ -207,9 +210,170 @@ static int ha_calc_timeout(struct pp_instance *ppi)
 	return pp_next_delay_1(ppi, HA_TO_TX);
 }
 
+/*
+ * Following functions are mainly copied from proto-ext-wr, with small changes
+ * We are *not* using most wrp-> fields. See ha-api.h for a check
+ */
+static int wr_handle_preq(struct pp_instance *ppi)
+{
+	ppi->received_ptp_header.correctionfield.msb = 0;
+	ppi->received_ptp_header.correctionfield.lsb =
+		phase_to_cf_units(ppi->last_rcv_time.phase);
+	return 0;
+}
+
+static int wr_master_msg(struct pp_instance *ppi, unsigned char *pkt, int plen,
+			 int msgtype)
+{
+	MsgHeader *hdr = &ppi->received_ptp_header;
+	TimeInternal *time = &ppi->last_rcv_time;
+
+	pp_diag(ppi, ext, 2, "hook: %s\n", __func__);
+
+	switch (msgtype) {
+
+	/* This case is modified from the default one */
+	case PPM_DELAY_REQ:
+		hdr->correctionfield.msb = 0;
+		hdr->correctionfield.lsb =
+			phase_to_cf_units(ppi->last_rcv_time.phase);
+		msg_issue_delay_resp(ppi, time); /* no error check */
+		msgtype = PPM_NOTHING_TO_DO;
+		break;
+
+	case PPM_PDELAY_REQ:
+		wr_handle_preq(ppi);
+		msgtype = PPM_NOTHING_TO_DO;
+		break;
+
+		/* No signaling stuff copied over from WR */
+	}
+
+	return msgtype;
+}
+
+static int wr_new_slave(struct pp_instance *ppi, unsigned char *pkt, int plen)
+{
+	pp_diag(ppi, ext, 2, "hook: %s\n", __func__);
+	wr_servo_init(ppi);
+	return 0;
+}
+
+static int wr_handle_resp(struct pp_instance *ppi)
+{
+	MsgHeader *hdr = &ppi->received_ptp_header;
+	TimeInternal correction_field;
+	TimeInternal *ofm = &DSCUR(ppi)->offsetFromMaster;
+	struct wr_dsport *wrp = WR_DSPOR(ppi);
+
+	pp_diag(ppi, ext, 2, "hook: %s\n", __func__);
+
+	/* FIXME: check sub-nano relevance of correction filed */
+	cField_to_TimeInternal(&correction_field, hdr->correctionfield);
+
+	/*
+	 * If no WR mode is on, run normal code, if T2/T3 are valid.
+	 * After we adjusted the pps counter, stamps are invalid, so
+	 * we'll have the Unix time instead, marked by "correct"
+	 */
+	if (!wrp->wrModeOn) {
+		if (!ppi->t2.correct || !ppi->t3.correct) {
+			pp_diag(ppi, servo, 1,
+				"T2 or T3 incorrect, discarding tuple\n");
+			return 0;
+		}
+		pp_servo_got_resp(ppi);
+		/*
+		 * pps always on if offset less than 1 second,
+		 * until ve have a configurable threshold */
+		if (ofm->seconds)
+			wrp->ops->enable_timing_output(ppi, 0);
+		else
+			wrp->ops->enable_timing_output(ppi, 1);
+
+	}
+	wr_servo_got_delay(ppi, hdr->correctionfield.lsb);
+	wr_servo_update(ppi);
+	return 0;
+}
+
+/* Hmm... "execute_slave" should look for errors; but it's off in WR too */
+static int wr_handle_followup(struct pp_instance *ppi,
+			      TimeInternal *precise_orig_timestamp,
+			      TimeInternal *correction_field)
+{
+	struct wr_dsport *wrp = WR_DSPOR(ppi);
+
+	pp_diag(ppi, ext, 2, "hook: %s\n", __func__);
+	if (wrp->L1SyncState != L1SYNC_UP)
+		return 0;
+
+	precise_orig_timestamp->phase = 0;
+	wr_servo_got_sync(ppi, precise_orig_timestamp,
+			  &ppi->t2);
+
+	if (ppi->mech == PP_P2P_MECH)
+		wr_servo_update(ppi);
+
+	return 1; /* the caller returns too */
+}
+
+static int wr_handle_presp(struct pp_instance *ppi)
+{
+	MsgHeader *hdr = &ppi->received_ptp_header;
+	TimeInternal correction_field;
+	struct wr_dsport *wrp = WR_DSPOR(ppi);
+	TimeInternal *ofm = &DSCUR(ppi)->offsetFromMaster;
+
+	/* FIXME: check sub-nano relevance of correction filed */
+	cField_to_TimeInternal(&correction_field, hdr->correctionfield);
+
+	/*
+	 * If no WR mode is on, run normal code, if T2/T3 are valid.
+	 * After we adjusted the pps counter, stamps are invalid, so
+	 * we'll have the Unix time instead, marked by "correct"
+	 */
+
+	if (!wrp->wrModeOn) {
+		if (!ppi->t3.correct || !ppi->t6.correct) {
+			pp_diag(ppi, servo, 1,
+				"T3 or T6 incorrect, discarding tuple\n");
+			return 0;
+		}
+		pp_servo_got_presp(ppi);
+		/*
+		 * pps always on if offset less than 1 second,
+		 * until ve have a configurable threshold */
+		if (ofm->seconds)
+			wrp->ops->enable_timing_output(ppi, 0);
+		else
+			wrp->ops->enable_timing_output(ppi, 1);
+
+		return 0;
+	}
+
+	ppi->t4_cf = hdr->correctionfield.lsb;
+	wr_servo_got_delay(ppi, ppi->t4_cf);
+	return 0;
+}
+
+
+/* The global structure used by ppsi */
 struct pp_ext_hooks pp_hooks = {
 	.open = ha_open, /* misnamed: global init  */
 	.init = ha_init, /* misnamed: port open */
 	.handle_signaling = ha_handle_signaling,
 	.calc_timeout = ha_calc_timeout,
+
+	/*
+	 * All the rest is wr-derived, to run the WR servo
+	 */
+	.master_msg = wr_master_msg,
+	.new_slave = wr_new_slave,
+	.handle_resp = wr_handle_resp,
+	.handle_followup = wr_handle_followup,
+	.handle_preq = wr_handle_preq,
+	.handle_presp = wr_handle_presp,
+
+
 };
