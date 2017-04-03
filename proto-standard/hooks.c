3cfdacc8c86ac2c9184222ae7aeaf9fb3a665a8a
pcm <paul.murray@novatechweb.com>
2017-04-03 10:10:34 -0400

    __attribute__((weak) ignored by MetroWerks ppc c compiler

--- proto-standard/hooks.c
+++ proto-standard/hooks.c
@@ -1,4 +1,6 @@
 #include <ppsi/ppsi.h>
 
 /* proto-standard offers all-null hooks as a default extension */
-struct pp_ext_hooks  __attribute__((weak)) pp_hooks;
+//__attribute__((weak) ignored by MetroWerks eppc c compiler
+//struct pp_ext_hooks  __attribute__((weak)) pp_hooks;  /* does not work */
+struct pp_ext_hooks  __declspec(weak) pp_hooks;         /* works */
